15b9e4a9160b418a8f6d5dba7f21e21f4c9817ab
Anita Rodney <anotarodney30@gmail.com>
2025-06-11 12:29:54 +0100

    hero page responsive

--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
 import'./output.css'
 
-const navBtn = document.querySelector('#menu');
-const menuBar = document.querySelector('[role="menubar"]');
+    const navBtn = document.querySelector('#menu');
+    const menuBar = document.querySelector('#mobileMenu');
 
-navBtn.addEventListener('click', () => {
-  const isExpanded = JSON.parse(navBtn.getAttribute('aria-expanded'));
-  navBtn.setAttribute('aria-expanded', !isExpanded);
-  menuBar.classList.toggle('hidden');
-  menuBar.classList.toggle('flex');
-})
+    navBtn.addEventListener('click', () => {
+      const isExpanded = navBtn.getAttribute('aria-expanded') === 'true';
+      navBtn.setAttribute('aria-expanded', !isExpanded);
+      menuBar.classList.toggle('hidden');
+      menuBar.classList.toggle('flex');
+    });
+ 
